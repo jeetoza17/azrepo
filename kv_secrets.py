8df0b2901c800a90fef5e8a17c0162b9a95b7ec3
jeetoza17 <100187056+jeetoza17@users.noreply.github.com>
2023-03-01 12:13:16 +0000

    Add files via upload

---
+++ kv_secrets.py
@@ -0,0 +1,23 @@
+import os
+from azure.keyvault.secrets import SecretClient
+from azure.identity import DefaultAzureCredential
+
+keyVaultName = "ozakeyvault"
+KVUri = f"https://ozakeyvault.vault.azure.net"
+
+credential = DefaultAzureCredential()
+client = SecretClient(vault_url=KVUri, credential=credential)
+
+secretName = input("Input a name for your secret > ")
+secretValue = input("Input a value for your secret > ")
+
+print(f"Creating a secret in {keyVaultName} called '{secretName}' with the value '{secretValue}' ...")
+
+client.set_secret(secretName, secretValue)
+
+print(" done.")
+
+print(f"Retrieving your secret from {keyVaultName}.")
+
+retrieved_secret = client.get_secret(secretName)
+
